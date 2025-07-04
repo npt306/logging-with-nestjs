0b54201ade51b0cf4cd79ffbabf6ffae95d7a392
npt306 <nptai212@clc.fitus.edu.vn>
2025-07-04 15:07:15 +0700

    create module logger, register filter and interceptor in app module instead of main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from './common/logger/logger.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggingInterceptor } from './common/interceptors/logging.interceptor';
+import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 
 @Module({
   imports: [
     UsersModule,
+    LoggerModule,
     ConfigModule.forRoot({
       envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
       isGlobal: true,
-
     }),
+
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: LoggingInterceptor
+    },
+    {
+      provide: APP_FILTER,
+      useClass: AllExceptionsFilter
+    }
+  ],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,6 @@ import { ErrorLogger } from './common/logger/error.logger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const httpLogger = new HttpLogger();
-  const errorLogger = new ErrorLogger();
-  const configService = app.get(ConfigService);
-
-  app.useGlobalInterceptors(new LoggingInterceptor(httpLogger, errorLogger));
-  app.useGlobalFilters(new AllExceptionsFilter(errorLogger, configService));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
